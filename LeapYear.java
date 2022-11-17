a17f6617871ae9b9f77f16286abbf60934844862
Geethanjali-Mathiyalagan <92878010+Geethanjali-Mathiyalagan@users.noreply.github.com>
2022-11-17 22:08:36 +0530

    Looping
    Loop Condition

---
+++ LeapYear.java
@@ -0,0 +1,22 @@
+package com.day17.www;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Year");
+		 int year = sc.nextInt();
+		
+	if(year%4==0)
+	{
+		System.out.println("It is a leap year");
+	}
+	else
+	{
+		System.out.println("It is not a leap year");
+	}
+
+}
+}
---
+++ PrintingNumbers.java
@@ -0,0 +1,24 @@
+package com.day17.www;
+
+public class PrintingNumbers {
+
+	public static void main(String[] args) {
+		int i=10;
+		while(i>=1)
+		{
+			System.out.println(i);
+			i--;
+		}
+		{
+			System.out.println("1 to 10");
+		}
+		while(i<=10)
+		{
+			System.out.println(i);
+		    i++;
+		}
+			
+		
+	}
+
+}
